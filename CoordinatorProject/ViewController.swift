7e5a9927fd029719f319383ca71875a6f2a2d2bd
elhoussaineelgamouz <eelgamouz.ext@deltadore.com>
2024-07-18 15:58:22 +0100

    Initial Commit

---
+++ CoordinatorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorProject
+//
+//  Created by El houssaine El GAMOUZ on 18/7/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
